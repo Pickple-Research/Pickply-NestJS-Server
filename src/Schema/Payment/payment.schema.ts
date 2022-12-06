cb35cf53dbc2a037be5ffe238dd8f4d94b4362fb
dennis2311 <dennis2311@daum.net>
2022-12-06 13:29:10 +0900

    (v1.1.18) 주문/결제 정보 스키마에 detail 속성 추가

--- src/Schema/Payment/payment.schema.ts
+++ src/Schema/Payment/payment.schema.ts
@@ -16,15 +16,17 @@ export class Payment {
   @Prop({ required: true }) // 결제 금액
   amount: number;
 
+  @Prop({ required: true }) // 결제 사유
+  reason: string;
+
+  @Prop() // 상세 내역 (리서치 제목 등)
+  detail?: string;
+
+  @Prop({ required: true, index: true }) // 결제자 _id
+  userId: string;
+
   @Prop({ required: true }) // 결제일자
   createdAt: string;
-
-  //* 아래 Prop 들은 결제 내역에 필수적인 요소들이지만 처음 웹훅을 통해 데이터를 생성할 때에는 전달되지 않으므로 optional 하게 설정합니다.
-  @Prop() // 결제 사유
-  reason?: string;
-
-  @Prop({ index: true }) // 결제자 _id
-  userId?: string;
 
   @Prop() // 결제 관련 리서치 _id
   researchId?: string;
