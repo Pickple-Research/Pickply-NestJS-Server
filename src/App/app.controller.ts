355644b1ce64647d2045b8de616b5a84ce581bb9
dennis2311 <dennis2311@daum.net>
2022-12-23 16:43:24 +0900

    (v1.1.22) 고정 투표 추가

--- src/App/app.controller.ts
+++ src/App/app.controller.ts
@@ -28,7 +28,7 @@ export class AppController {
   @Public()
   @Get("release")
   async test() {
-    return "2022-12-22 1022 v1.1.22";
+    return "2022-12-23 1643 v1.1.22";
   }
 
   /**
--- src/Object/Type/enum.type.ts
+++ src/Object/Type/enum.type.ts
@@ -1,3 +1,8 @@
+/**
+ * DB 에 저장될 실제 값(value)과 앱에서 표시할 이름(displayName) 으로 구성되는 Type.
+ * 주로 서버와 동기화되는 상수를 정의할 때 사용합니다.
+ * @author 현웅
+ */
 export type EnumValueWithName = {
   value: string;
   displayName: string;
