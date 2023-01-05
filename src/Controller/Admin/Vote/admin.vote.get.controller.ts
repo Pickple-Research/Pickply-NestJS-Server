e5ff1f9431bd4c0b3114167f2d734266d4a78011
OptimusLee <36293518+tmddnjs2385@users.noreply.github.com>
2023-01-05 14:47:51 +0000

    투표 목록 불러오기, 상세 정보, 댓글 가져오기

--- src/Controller/Admin/Vote/admin.vote.get.controller.ts
+++ src/Controller/Admin/Vote/admin.vote.get.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Inject, Get } from "@nestjs/common";
-import { Roles } from "src/Security/Metadata";
+import { Controller, Inject, Get, Param } from "@nestjs/common";
+import { Public, Roles } from "src/Security/Metadata";
 import {
   MongoUserFindService,
   MongoVoteFindService,
   MongoVoteStandardizeService,
 } from "src/Mongo";
 import { UserType } from "src/Object/Enum";
+import { encrypt } from "src/Util/crypto.util";
 
 /**
  * 관리자만 사용하는 투표 관련 Get 컨트롤러입니다.
@@ -13,7 +14,7 @@ import { UserType } from "src/Object/Enum";
  */
 @Controller("admin/votes")
 export class AdminVoteGetController {
-  constructor() {}
+  constructor() { }
 
   @Inject()
   private readonly mongoUserFindService: MongoUserFindService;
@@ -21,6 +22,55 @@ export class AdminVoteGetController {
   private readonly mongoVoteFindService: MongoVoteFindService;
   @Inject()
   private readonly mongoVoteStandardizeService: MongoVoteStandardizeService;
+
+
+
+
+  /**
+   * @author 승원
+   * 모든 투표를 가져옵니다.
+   * 그린라이트일 경우, 작성자가 익명으로 나옴
+   * 추후에 service에서 처리할 예정
+   */
+  //@Public()
+  @Roles(UserType.ADMIN)
+  @Get("")
+  async getVotes() {
+    const votes = await this.mongoVoteFindService.getVotes({});
+    return encrypt(votes);
+  }
+
+
+  /**
+   * @author 승원
+   * 특정 투표를 가져옵니다.
+   * 그린라이트일 경우, 작성자가 익명으로 나옴
+   * 추후에 service에서 처리할 예정
+   */
+
+  //@Public()
+  @Roles(UserType.ADMIN)
+  @Get(":voteId")
+  async getVote(@Param("voteId") voteId: string) {
+    const vote = await this.mongoVoteFindService.getVoteById(voteId);
+    return encrypt(vote);
+  }
+
+  /**
+   * @author: 승원
+   * 특정 투표에 대한 (대)댓글을 가져옵니다.
+   * 그린라이트일 경우, 작성자가 익명으로 나옴
+   * 추후에 service에서 처리할 예정
+   */
+
+  //@Public()
+  @Roles(UserType.ADMIN)
+  @Get(":voteId/comments")
+  async getVoteComments(@Param("voteId") voteId: string) {
+    const comments = await this.mongoVoteFindService.getVoteComments(voteId);
+    return encrypt(comments);
+  }
+
 
   /**
    * 투표 관련 정보를 일괄적으로 변경합니다. 그 때마다 맞춰서 사용합니다.
