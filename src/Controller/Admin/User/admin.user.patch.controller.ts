7f87f5a61c550f11426402a20a67dd059d609bf1
optimusLee2385 <120026860+optimusLee2385@users.noreply.github.com>
2023-01-08 09:57:33 +0000

    유저 기본 정보와 유저의 특성 정보 수정하는 컨트롤러 추가

--- src/Controller/Admin/User/admin.user.patch.controller.ts
+++ src/Controller/Admin/User/admin.user.patch.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Body, Patch } from "@nestjs/common";
+import { Controller, Inject, Body, Patch, Param } from "@nestjs/common";
 import { Roles } from "src/Security/Metadata";
 import { MongoUserUpdateService } from "src/Mongo";
 import { AuthService } from "src/Service";
@@ -10,12 +10,75 @@ import { UserType } from "src/Object/Enum";
  */
 @Controller("admin/users")
 export class AdminUserPatchController {
-  constructor() {}
+  constructor() { }
 
   @Inject()
   private readonly authService: AuthService;
   @Inject()
   private readonly mongoUserUpdateService: MongoUserUpdateService;
+
+  /**
+   * @author 승원
+   * 
+   * 특정 유저의 기본 정보와 특성 정보를 수정함
+   * 날짜와 관련된 정보가 바로 바로 수정되서 
+   * 클라이언트로 넘어오지 않음.
+   * DB에서는 수정된 정보가 존재함.
+   * 
+   */
+
+  @Roles(UserType.ADMIN)
+  @Patch(":userId")
+  async updateUser(
+    @Param("userId") userId: string,
+    @Body() body: {
+      nickname?: string;
+      email?: string;
+      gender?: string;
+      createdAt?: string;
+      birthday?: string;
+      credit?: number;
+    }) {
+
+    //return console.log(body)
+
+    const editUser = await this.mongoUserUpdateService.updateUser({
+      userId,
+      updateQuery: {
+        $set: {
+          nickname: body.nickname,
+          email: body.email,
+          createdAt: body.createdAt,
+          credit: body.credit
+        }
+      }
+    })
+
+    const editUserProperty = await this.mongoUserUpdateService.updateUserPropertyById({
+      userId,
+      updateQuery: {
+        $set: {
+          gender: body.gender,
+          birthday: body.birthday
+
+        }
+      }
+    })
+
+    const user = await Promise.all([editUser, editUserProperty]).then(
+      ([user, userProperty]) => {
+        return {
+          user,
+          userProperty
+        }
+      },
+    );
+
+    //console.log(user)
+
+    return user;
+
+  }
 
   /**
    * 다수 유저의 정보를 일괄적으로 변경합니다.
